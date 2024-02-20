699ab5b3bb50769a8a89720b11d2bd796e75ae81
rajatasusual <krajat4@gmail.com>
2024-02-20 01:45:29 +0000

    Day 51

---
+++ leetCode/daily_questions/2_20_missingNumber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+
+    return nums.length * (nums.length + 1) / 2 - sum;
+};
+
+console.log(missingNumber([3,0,1]));
+console.log(missingNumber([0,1]));
